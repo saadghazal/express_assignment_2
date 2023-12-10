a70b2c1fadce90b66d609d3553257b0ad08c4938
saadghazal <saad.m.ghazal@icloud.com>
2023-12-11 01:20:40 +0300

    creating home route

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const usersRoute = require('./routes/users')
+const homeRoute = require('./routes/home')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(usersRoute)
 
-
+app.use(homeRoute)
 
 
 app.listen(3000)
