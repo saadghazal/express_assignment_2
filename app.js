8377ab1bc8a23327d9c6c85318e61965d799fa09
saadghazal <saad.m.ghazal@icloud.com>
2023-12-11 01:30:23 +0300

    access static files

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const usersRoute = require('./routes/users')
 const homeRoute = require('./routes/home')
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname,'public')))// it will access static files that should be included in the html pages for example like a css files
+
 
 app.use(usersRoute)
 
