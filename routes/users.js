0a972c4221cb145d82ffcbc097a1673c7d1e68ea
saadghazal <saad.m.ghazal@icloud.com>
2023-12-11 01:26:59 +0300

    create home page with parsing user input

--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,9 @@ router.get('/users',(req,res,next)=>{
     res.sendFile(path.join(__dirname,'../','views','users.html'))
 
 })
+router.post('/users',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
 
 module.exports = router
