d3f40254bd3c880802a84a642e963bde1fcd484e
saadghazal <saad.m.ghazal@icloud.com>
2023-12-11 01:14:14 +0300

    create users html page

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const usersRoute = require('./routes/users')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(usersRoute)
+
+
 
 
 app.listen(3000)
--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,8 @@ const router = express.Router()
 
 
 router.get('/users',(req,res,next)=>{
+    res.sendFile(path.join(__dirname,'../','views','users.html'))
 
 })
+
+module.exports = router
